1af7a2aa54e73812a179fb78c9d37fe086530f54
snigbar <snigbar@outlook.com>
2025-05-18 14:43:48 +0600

    create separate cart modal and finalize

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import {
-  clearCart,
-  decreaseQuantity,
-  increaseQuantity,
-  removeFromCart,
-} from "../store/cartSlice";
 import type { CartItem, RootState } from "../interfaces/Cart";
 import { useEffect, useRef, useState } from "react";
+import CartDropdown from "./CartDropdown";
 
 function Header() {
   const cartItems = useSelector((state: RootState) => state.cart.items);
@@ -18,7 +13,6 @@ function Header() {
     0
   );
 
-  const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
@@ -91,86 +85,11 @@ function Header() {
         </button>
 
         {isOpen && (
-          <div
-            ref={modalRef}
-            className="absolute right-0 mt-2 w-80 bg-white text-black shadow-lg rounded p-4 z-10 max-h-[80vh] scroll-auto overflow-y-auto"
-          >
-            <div className="flex justify-between items-center mb-2">
-              <h2 className="text-lg font-bold mb-2">Cart Items</h2>
-              <button
-                onClick={() => setIsOpen(false)}
-                className="text-gray-500 hover:text-gray-700"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              </button>
-            </div>
-            {cartItems.length === 0 ? (
-              <p className="text-sm">Cart is empty.</p>
-            ) : (
-              <div className="space-y-2">
-                {cartItems.map((item) => (
-                  <div key={item.id} className="border-b pb-2">
-                    <p className="font-medium">{item.title}</p>
-                    <p className="text-sm">Qty: {item.quantity}</p>
-                    <div className="flex space-x-2 mt-1">
-                      <button
-                        onClick={() => dispatch(increaseQuantity(item.id))}
-                        className="bg-green-500 text-white px-2 rounded"
-                      >
-                        <span className="">+</span>
-                      </button>
-                      <button
-                        onClick={() => dispatch(decreaseQuantity(item.id))}
-                        className="bg-yellow-500 text-white px-2 rounded"
-                      >
-                        <span className="">-</span>
-                      </button>
-                      <button
-                        onClick={() => dispatch(removeFromCart(item.id))}
-                        className="bg-red-500 text-white px-2 rounded"
-                      >
-                        <span className="">x</span>
-                      </button>
-                    </div>
-                  </div>
-                ))}
-                {/* Total Price */}
-                <div className="mt-3 font-semibold text-right">
-                  Total: ${totalPrice.toFixed(2)}
-                </div>
-
-                {/* View Cart Link */}
-                <Link
-                  to="/cart"
-                  className="mt-2 block text-center bg-blue-600 text-white py-1 rounded hover:bg-blue-700"
-                  onClick={() => setIsOpen(false)}
-                >
-                  View Full Cart
-                </Link>
-
-                {/* Clear Cart */}
-                <button
-                  onClick={() => dispatch(clearCart())}
-                  className="mt-2 w-full bg-red-600 text-white py-1 rounded hover:bg-red-700"
-                >
-                  Clear Cart
-                </button>
-              </div>
-            )}
-          </div>
+          <CartDropdown
+            cartItems={cartItems}
+            totalPrice={totalPrice}
+            setIsOpen={setIsOpen}
+          />
         )}
       </div>
     </nav>
